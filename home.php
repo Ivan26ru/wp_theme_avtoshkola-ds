922c4fcf17f0a73b774e9aecbbf36eeab7231e10
i.pisarenko <i.pisarenko@crmguru.ru>
2022-03-28 13:29:21 +0300

    add include block_offers.php

--- home.php
+++ home.php
@@ -319,39 +319,7 @@ ob_start("fill_alt");
     </div>
     </div>
 </section>
-<!-- Offers -->
-    <section class="offers" id="offers">
-    <div class="container">
-        <div class="offer">
-            <p class="offer__title title">
-                Спецпредложения и акции
-            </p>
-            <div class="offer__cont">
-                <?php if( have_rows('offer', 'option') ): ?>
-                    <?php while( have_rows('offer', 'option') ): the_row();
-                        $name = get_sub_field('offer_title', 'option');
-                        $text = get_sub_field('offer_text', 'option');
-                        $link = get_sub_field('offer_link', 'option');
-                        $img = get_sub_field('offer_img', 'option');
-                        ?>
-                        <a href="<?php echo $link; ?>" class="offer__item">
-                            <img src="<?php echo $img; ?>" alt="" class="offer__photo"/>
-                            <p class="offer__name">
-                                <?php echo $name; ?>
-                            </p>
-                            <p class="offer__text">
-                                <?php echo $text; ?>
-                            </p>
-                            <button class="offer__btn btn form-up">
-                                Подробнее
-                            </button>
-                        </a>
-                    <?php endwhile; ?>
-                <?php endif; ?>
-            </div>
-        </div>
-    </div>
-</section>
+	<?php include "inc/block_offers.php"; ?>
 <!-- Instructors -->
     <section class="instructors" id="instructors">
     <div class="container">
