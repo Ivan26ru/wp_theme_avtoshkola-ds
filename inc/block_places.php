f3cc3ef2819764ddf5fedcdde26cd1c15127c52c
i.pisarenko <i.pisarenko@crmguru.ru>
2022-03-28 11:53:09 +0300

    add include block_places.php

---
+++ inc/block_places.php
@@ -0,0 +1,22 @@
+<!--Places-->
+<section class="places" id="places">
+	<div class="container">
+		<div class="place">
+			<p class="place__title title">Наши площадки для вождения</p>
+			<div class="place__cont">
+				<?php if( have_rows('place', 'option') ): ?>
+					<?php while( have_rows('place', 'option') ): the_row();
+						$name = get_sub_field('place_title', 'option');
+						$link = get_sub_field('place_link', 'option');
+						$img = get_sub_field('place_img', 'option');
+						?>
+						<a href="<?php echo $link; ?>" class="place__item">
+							<img src="<?php echo $img; ?>" alt="" class="offer__photo"/>
+							<span><?php echo $name; ?></span>
+						</a>
+					<?php endwhile; ?>
+				<?php endif; ?>
+			</div>
+		</div>
+	</div>
+</section>
--- single.php
+++ single.php
@@ -93,29 +93,7 @@ ob_start("fill_alt");
             </div>
         </div>
     </section>
-<!--Places-->
-    <section class="places" id="places">
-        <div class="container">
-            <div class="place">
-                <p class="place__title title">Наши площадки для вождения</p>
-                <div class="place__cont">
-                    <?php if( have_rows('place', 'option') ): ?>
-                        <?php while( have_rows('place', 'option') ): the_row();
-                            $name = get_sub_field('place_title', 'option');
-                            $link = get_sub_field('place_link', 'option');
-                            $img = get_sub_field('place_img', 'option');
-                            ?>
-                            <a href="<?php echo $link; ?>" class="place__item">
-                                <img src="<?php echo $img; ?>" alt="" class="offer__photo"/>
-                                <span><?php echo $name; ?></span>
-                            </a>
-                        <?php endwhile; ?>
-                    <?php endif; ?>
-                </div>
-            </div>
-        </div>
-    </section>
-<!-- Licenses -->
+<?php include "inc/block_places.php"; ?><!-- Licenses -->
     <section class="licenses" id="licenses">
         <div class="container">
             <div class="license">
