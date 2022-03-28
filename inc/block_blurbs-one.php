f36142b70db3e5438d76ccb1db87b191fb72508d
i.pisarenko <i.pisarenko@crmguru.ru>
2022-03-28 11:49:41 +0300

    add include block_blurbs-one.php

---
+++ inc/block_blurbs-one.php
@@ -0,0 +1,38 @@
+<!--Blurbs-->
+<section class="blurbs" id="blurbs-one">
+	<div class="container">
+		<div class="blurb">
+			<div class="blurb__up">
+				<div class="blurb__info">
+					<span>Акция действует до 03.02.22</span>
+					<span>Времени осталось:  2 дн. 16 час. 34 мин. 20 сек.</span>
+					<div class="blurb__item">
+						<div>
+							<p class="name">ТЕОРИЯ</p>
+							<p class="big red">10.990</p>
+						</div>
+						<span>+</span>
+						<div>
+							<p class="name">ГИБДД</p>
+							<p class="big">8.000 <span>руб.</span></p>
+						</div>
+						<span>+</span>
+						<div>
+							<p class="name">ВОЖДЕНИЕ</p>
+							<p class="big">7.990 <span>руб.</span></p>
+						</div>
+						<span>=</span>
+						<div>
+							<p class="name">ИТОГО</p>
+							<p class="big">15.990 <span>руб.</span></p>
+						</div>
+					</div>
+				</div>
+				<div class="blurb__form">
+					<p>Текущая акция</p>
+					<?php echo do_shortcode('[contact-form-7 id="2062" title="Текущая акция"]')?>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
