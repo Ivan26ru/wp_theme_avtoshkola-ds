b72f16c6c0ac5aa51e36e50a5125caa4dee3a0a0
i.pisarenko <i.pisarenko@crmguru.ru>
2022-03-28 11:43:36 +0300

    add include bock_left-sidebar.php

---
+++ inc/block_left-sidebar.php
@@ -0,0 +1,61 @@
+<div class="sidebar">
+	<div class="sidebar__fixed" id="sidebar-fixed">
+		<ul class="sidebar__list">
+			<?php if( have_rows('menu', 'option') ): ?>
+				<?php while( have_rows('menu', 'option') ): the_row();
+					$text = get_sub_field('menu_text', 'option');
+					$link = get_sub_field('menu_link', 'option');
+					?>
+					<li>
+						<a href="<?php echo $link; ?>">
+							<p><?php echo $text; ?></p>
+						</a>
+					</li>
+				<?php endwhile; ?>
+			<?php endif; ?>
+		</ul>
+		<ul class="sidebar__list">
+			<?php if( have_rows('menu-two', 'option') ): ?>
+				<?php while( have_rows('menu-two', 'option') ): the_row();
+					$text = get_sub_field('menu_text-two', 'option');
+					$link = get_sub_field('menu_link-two', 'option');
+					?>
+					<li>
+						<a href="<?php echo $link; ?>">
+							<p><?php echo $text; ?></p>
+						</a>
+					</li>
+				<?php endwhile; ?>
+			<?php endif; ?>
+		</ul>
+		<ul class="sidebar__list">
+			<?php if( have_rows('menu-three', 'option') ): ?>
+				<?php while( have_rows('menu-three', 'option') ): the_row();
+					$text = get_sub_field('menu_text-three', 'option');
+					$link = get_sub_field('menu_link-three', 'option');
+					?>
+					<li>
+						<a href="<?php echo $link; ?>">
+							<p><?php echo $text; ?></p>
+						</a>
+					</li>
+				<?php endwhile; ?>
+			<?php endif; ?>
+		</ul>
+		<div class="sidebar__contact">
+			<img src="<?php bloginfo('template_directory')?>/img/map-littel.png" alt="" class="sidebar__map"/>
+			<p class="sidebar__place">Центральный офис: Москва, ул. Степана Супруна дом 4</p>
+			<a href="tel:89995991615" class="sidebar__phone">
+				<p>8-999-599-1615</p>
+			</a>
+			<p class="sidebar__time">
+				<a href="tel:88007587702" ><img src="<?php bloginfo('template_directory')?>/img/telegram.svg" alt="Телефон в шапке"/></a>
+				<a href="tel:88007587702" ><img src="<?php bloginfo('template_directory')?>/img/whatsap.svg" alt="Телефон в шапке"/></a>
+				c 8:00 до 20:00
+			</p>
+			<a href="mailto:info@985.su" class="sidebar__email">
+				<p>info@uc-avd.ru</p>
+			</a>
+		</div>
+	</div>
+</div>
--- single.php
+++ single.php
@@ -121,67 +121,7 @@ ob_start("fill_alt");
         <div class="container">
             <div class="take-info">
                 <div class="take-info__cont">
-                    <div class="sidebar">
-                        <div class="sidebar__fixed" id="sidebar-fixed">
-                            <ul class="sidebar__list">
-                                <?php if( have_rows('menu', 'option') ): ?>
-                                    <?php while( have_rows('menu', 'option') ): the_row();
-                                        $text = get_sub_field('menu_text', 'option');
-                                        $link = get_sub_field('menu_link', 'option');
-                                        ?>
-                                        <li>
-                                            <a href="<?php echo $link; ?>">
-                                                <p><?php echo $text; ?></p>
-                                            </a>
-                                        </li>
-                                    <?php endwhile; ?>
-                                <?php endif; ?>
-                            </ul>
-                            <ul class="sidebar__list">
-                                <?php if( have_rows('menu-two', 'option') ): ?>
-                                    <?php while( have_rows('menu-two', 'option') ): the_row();
-                                        $text = get_sub_field('menu_text-two', 'option');
-                                        $link = get_sub_field('menu_link-two', 'option');
-                                        ?>
-                                        <li>
-                                            <a href="<?php echo $link; ?>">
-                                                <p><?php echo $text; ?></p>
-                                            </a>
-                                        </li>
-                                    <?php endwhile; ?>
-                                <?php endif; ?>
-                            </ul>
-                            <ul class="sidebar__list">
-                                <?php if( have_rows('menu-three', 'option') ): ?>
-                                    <?php while( have_rows('menu-three', 'option') ): the_row();
-                                        $text = get_sub_field('menu_text-three', 'option');
-                                        $link = get_sub_field('menu_link-three', 'option');
-                                        ?>
-                                        <li>
-                                            <a href="<?php echo $link; ?>">
-                                                <p><?php echo $text; ?></p>
-                                            </a>
-                                        </li>
-                                    <?php endwhile; ?>
-                                <?php endif; ?>
-                            </ul>
-                            <div class="sidebar__contact">
-                                <img src="<?php bloginfo('template_directory')?>/img/map-littel.png" alt="" class="sidebar__map"/>
-                                <p class="sidebar__place">Центральный офис: Москва, ул. Степана Супруна дом 4</p>
-                                <a href="tel:89995991615" class="sidebar__phone">
-                                    <p>8-999-599-1615</p>
-                                </a>
-                                <p class="sidebar__time">
-                                    <a href="tel:88007587702" ><img src="<?php bloginfo('template_directory')?>/img/telegram.svg" alt="Телефон в шапке"/></a>
-                                    <a href="tel:88007587702" ><img src="<?php bloginfo('template_directory')?>/img/whatsap.svg" alt="Телефон в шапке"/></a>
-                                    c 8:00 до 20:00
-                                </p>
-                                <a href="mailto:info@985.su" class="sidebar__email">
-                                    <p>info@uc-avd.ru</p>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
+					<?php include "inc/block_left-sidebar.php"; ?>
                     <div class="contant">
                         <?php if ( function_exists( 'dimox_breadcrumbs' ) ) dimox_breadcrumbs(); ?>
                         <?php the_content(); ?>
