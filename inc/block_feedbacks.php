7be1a22ca52ebf8b814bdde3975bf86933d508e0
i.pisarenko <i.pisarenko@crmguru.ru>
2022-03-31 13:55:17 +0300

    fix feedback

--- inc/block_feedbacks.php
+++ inc/block_feedbacks.php
@@ -6,12 +6,14 @@
 			<div class="feedback__cont">
 				<?php if( have_rows('feedback', 'option') ): ?>
 					<?php while( have_rows('feedback', 'option') ): the_row();
-						$name = get_sub_field('feedback_name', 'option');
-						$text = get_sub_field('feedback_text', 'option');
-						$img = get_sub_field('feedback_img', 'option');
+						$name = get_sub_field('feedback_name');
+						$text = get_sub_field('feedback_text');
+						$img = get_url_img_is_sub_field('feedback_img', 'thumb-94');
 						?>
 						<div><div class="feedback__item">
-								<img class="feedback__photo" src="<?php echo $img; ?>" alt="<?php echo $name; ?>"/>
+								<div class="feedback__item_wrap_img">
+									<img class="feedback__photo" src="<?php echo $img; ?>" alt="<?php echo $name; ?>"/>
+								</div>
 								<div class="feedback__info">
 									<p class="feedback__name"><?php echo $name; ?></p>
 									<p class="feedback__text"><?php echo $text; ?></p>
