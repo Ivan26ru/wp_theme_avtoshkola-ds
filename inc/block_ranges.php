644bb058ae1b0d8fc0c4e64e3f21278dfd298ea4
i.pisarenko <i.pisarenko@crmguru.ru>
2022-03-31 10:36:11 +0300

    fix get img post

--- inc/block_ranges.php
+++ inc/block_ranges.php
@@ -10,12 +10,15 @@
 				)); ?>
 				<?php while ($the_query->have_posts()) : $the_query->the_post(); ?><?php
 					$title = (get_field('auto_name')) ? get_field('auto_name') : get_the_title();
-					$img_src = (get_field('auto_img')) ? get_field('auto_img') : bloginfo('template_directory') . '/img/new.png';
+					//Проверка существования миниатюры и вставка либо ее, либо заглушки
+					$img = (has_post_thumbnail()) ? get_the_post_thumbnail($post->ID, 'thumb-94', array("class" => "range__photo")) : '<img src="' . get_bloginfo('template_directory') . '/img/new.png' . '" class="range__photo"/>';
 					?>
 					<div class="range__cont postid-<?php echo $post->ID; ?>">
 						<div class="range__item">
 							<div class="range__part">
-								<img src="<?php echo $img_src; ?>" class="range__photo"/>
+								<div class="range__part_wrap_img">
+									<?php echo $img; ?>
+								</div>
 								<div class="range__reting">
 									<?php if (have_rows('auto_reting')): ?><?php while (have_rows('auto_reting')): the_row();
 										$icon = get_sub_field('auto_icon', 'option');
