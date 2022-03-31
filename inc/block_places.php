0baa58ee8c64416071e26bb3fdd1e010ac80b81e
i.pisarenko <i.pisarenko@crmguru.ru>
2022-03-31 11:47:50 +0300

    fix img size to place

--- inc/block_places.php
+++ inc/block_places.php
@@ -4,18 +4,18 @@
 		<div class="place">
 			<p class="place__title title">Наши площадки для вождения</p>
 			<div class="place__cont">
-				<?php if( have_rows('place', 'option') ): ?>
-					<?php while( have_rows('place', 'option') ): the_row();
-						$name = get_sub_field('place_title', 'option');
-						$link = get_sub_field('place_link', 'option');
-						$img = get_sub_field('place_img', 'option');
-						?>
-						<a href="<?php echo $link; ?>" class="place__item">
+				<?php if (have_rows('place', 'option')): ?><?php while (have_rows('place', 'option')): the_row();
+					$name = get_sub_field('place_title', 'option');
+					$link = get_sub_field('place_link', 'option');
+					$img = wp_get_attachment_image_url(get_sub_field('place_img', 'option'), 'lic-207x285');
+					?>
+					<a href="<?php echo $link; ?>" class="place__item">
+						<div class="place__item_wrap_img">
 							<img src="<?php echo $img; ?>" title="<?php echo $name; ?>" alt="<?php echo $name; ?>" class="offer__photo"/>
-							<span><?php echo $name; ?></span>
-						</a>
-					<?php endwhile; ?>
-				<?php endif; ?>
+						</div>
+						<span><?php echo $name; ?></span>
+					</a>
+				<?php endwhile; ?><?php endif; ?>
 			</div>
 		</div>
 	</div>
