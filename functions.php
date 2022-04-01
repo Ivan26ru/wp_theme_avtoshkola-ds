d3d2f2ce0fd237f8daccb22ae500f1d5164d1c5b
i.pisarenko <i.pisarenko@crmguru.ru>
2022-04-01 10:50:46 +0300

    fix size img block offer

--- functions.php
+++ functions.php
@@ -451,6 +451,7 @@ add_image_size('thumb-94', 94, 94, true); // добавляем еще один 
 add_image_size('thumb-150', 150, 150, true); // добавляем еще один размер картинкам 94x94 с обрезкой
 add_image_size('lic-207x285', 207, 285, true);
 add_image_size('place-270x152', 270, 152, true);
+add_image_size('offer-320x135', 320, 135, true);
 add_image_size('instructors-173x173', 173, 173, true);
 
 // добавить возможность выбрать новый размер при вставке в запись
--- inc/block_offers.php
+++ inc/block_offers.php
@@ -3,30 +3,29 @@
 	<div class="container">
 		<div class="offer">
 			<p class="offer__title title">
-				Спецпредложения и акции
-			</p>
+				Спецпредложения и акции </p>
 			<div class="offer__cont">
-				<?php if( have_rows('offer', 'option') ): ?>
-					<?php while( have_rows('offer', 'option') ): the_row();
-						$name = get_sub_field('offer_title', 'option');
-						$text = get_sub_field('offer_text', 'option');
-						$link = get_sub_field('offer_link', 'option');
-						$img = get_sub_field('offer_img', 'option');
-						?>
-						<a href="<?php echo $link; ?>" class="offer__item">
-							<img src="<?php echo $img; ?>" title="<?php echo $name; ?>" alt="<?php echo $name; ?>" class="offer__photo"/>
-							<p class="offer__name">
-								<?php echo $name; ?>
-							</p>
-							<p class="offer__text">
-								<?php echo $text; ?>
-							</p>
-							<button class="offer__btn btn form-up">
-								Подробнее
-							</button>
-						</a>
-					<?php endwhile; ?>
-				<?php endif; ?>
+				<?php if (have_rows('offer', 'option')): ?><?php while (have_rows('offer', 'option')): the_row();
+					$name = get_sub_field('offer_title');
+					$text = get_sub_field('offer_text');
+					$link = get_sub_field('offer_link');
+					$img = get_url_img_is_sub_field('offer_img', 'offer-320x135');
+					?>
+					<a href="<?php echo $link; ?>" class="offer__item">
+						<div class="offer__item_wrap_img">
+							<img src="<?php echo $img; ?>" title="<?php echo $name; ?>" alt="<?php echo $name; ?>" />
+						</div>
+						<p class="offer__name">
+							<?php echo $name; ?>
+						</p>
+						<p class="offer__text">
+							<?php echo $text; ?>
+						</p>
+						<button class="offer__btn btn form-up">
+							Подробнее
+						</button>
+					</a>
+				<?php endwhile; ?><?php endif; ?>
 			</div>
 		</div>
 	</div>
